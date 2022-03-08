3300a39dee11f7ac051ddd3be9f67f6d6a932488
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2022-03-08 18:36:05 +0100

    use dockerize to wait for pg and keycloak

--- configurator/tf.sh
+++ configurator/tf.sh
@@ -2,15 +2,8 @@
 
 set -ex
 
-function waitport {
-    until printf "" 2>>/dev/null >>/dev/tcp/$1/$2; do
-        echo waiting for $1:$2 > /dev/stderr
-        sleep 1;
-    done
-}
-
 cd /tf
-waitport $KC_DB_URL_HOST $KC_DB_URL_PORT
+dockerize -timeout 60s -wait tcp://$KC_DB_URL_HOST:$KC_DB_URL_PORT
 terraform init -no-color --backend-config="conn_str=postgres://$KC_DB_USERNAME:$KC_DB_PASSWORD@$KC_DB_URL_HOST:$KC_DB_URL_PORT/$KC_DB_URL_DATABASE?sslmode=disable"
-waitport localhost 8080
+dockerize -timeout 60s -wait http://localhost:8080/auth/health
 terraform apply -no-color --auto-approve
