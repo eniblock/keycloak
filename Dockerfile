3e261ed58f63ef326ff6e2094ae671b75040c1bd
Gaëtan Lehmann <gaetan.lehmann@gmail.com>
2022-03-11 16:28:11 +0100

    run the configurator from its own container
    and react to the changes in the config files by re-running terraform

--- Dockerfile
+++ Dockerfile
@@ -1,3 +1,6 @@
+FROM alpine as reflex-downloader
+RUN wget -O - https://github.com/cespare/reflex/releases/download/v0.3.1/reflex_linux_amd64.tar.gz | tar xvzC /usr/bin --strip-components=1 reflex_linux_amd64/reflex
+
 FROM quay.io/keycloak/keycloak:17.0.0 as builder
 RUN /opt/keycloak/bin/kc.sh build \
   --metrics-enabled=true \
@@ -9,6 +12,7 @@ FROM quay.io/keycloak/keycloak:17.0.0
 USER root
 RUN curl -sSL https://github.com/powerman/dockerize/releases/download/v0.16.0/dockerize-linux-x86_64 > /usr/bin/dockerize \
   && chmod a+x /usr/bin/dockerize
+COPY --from=reflex-downloader /usr/bin/reflex /usr/bin/
 USER keycloak
 WORKDIR /opt/keycloak
 RUN keytool -genkeypair -storepass password -storetype PKCS12 -keyalg RSA -keysize 2048 -dname "CN=server" -alias server -ext "SAN:c=DNS:localhost,IP:127.0.0.1" -keystore conf/server.keystore
--- configurator/tf.sh
+++ configurator/tf.sh
@@ -1,17 +1,31 @@
 #!/bin/sh
 
-set -ex
+echo
+
+set -e
 
 cd /tf
+d=`mktemp -d`
+
+# copy the file in a tempdir, so we don't have to touch the files
+cp -a /tf/ $d/
+
+cd $d/tf
 
 # link the files in /tf/files, that comes from the config map, to /tf
-for f in `ls /tf/files`; do
-    ln -sf /tf/files/$f $f
+for f in `ls $d/tf/files`; do
+    ln -sf $d/tf/files/$f $f
 done
 
+export TF_IN_AUTOMATION=true
 dockerize -timeout 60s -wait tcp://$KC_DB_URL_HOST:$KC_DB_URL_PORT
 terraform init -no-color --backend-config="conn_str=postgres://$KC_DB_USERNAME:$KC_DB_PASSWORD@$KC_DB_URL_HOST:$KC_DB_URL_PORT/$KC_DB_URL_DATABASE?sslmode=disable"
 dockerize -timeout 60s -wait http://localhost:8080/auth/health
 export TF_VAR_admin_username=$KEYCLOAK_ADMIN
 export TF_VAR_admin_password=$KEYCLOAK_ADMIN_PASSWORD
 terraform apply -no-color --auto-approve -lock-timeout=60s
+
+cd /tf
+rm -rf $d
+
+echo
